3a5f50afbe900045a866bdf6423e3e8b7641bce2
user <uabj8000@gmail.com>
2020-11-08 19:31:04 -0300

    cliente repository

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,23 +1,22 @@
 package com.algaworks.osworks.api.controller;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        return manager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
+        return clienteRepository.findAll();
     }
 
 }
