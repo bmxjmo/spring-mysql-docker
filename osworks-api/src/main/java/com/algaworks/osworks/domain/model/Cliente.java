5e5e821849ac6ec50740b0efee9cc2cdba5dba69
user <uabj8000@gmail.com>
2020-11-08 19:53:42 -0300

    bean validation

--- osworks-api/src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ osworks-api/src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -1,6 +1,9 @@
 package com.algaworks.osworks.domain.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
@@ -9,9 +12,18 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
 
+    @NotBlank
+    @Size(max = 20)
     @Column(name = "fone")
     private String telefone;
 
