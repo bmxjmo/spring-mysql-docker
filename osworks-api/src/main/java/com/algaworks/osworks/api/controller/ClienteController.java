31500a04e9a98a2992d16f65003ea1fac5a27c30
user <uabj8000@gmail.com>
2020-11-08 19:23:07 -0300

    cliente model

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -4,29 +4,20 @@ import com.algaworks.osworks.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
+
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
-    public List<Cliente> listar(){
-
-        var cliente1 = new Cliente();
-        var cliente2 = new Cliente();
-
-        cliente1.setId(1L);
-        cliente1.setNome("John");
-        cliente1.setEmail("john@email.com");
-        cliente1.setTelefone("11 98765 4321");
-
-        cliente2.setId(1L);
-        cliente2.setNome("Paul");
-        cliente2.setEmail("paul@email.com");
-        cliente2.setTelefone("22 98765 4321");
-
-        return Arrays.asList(cliente1, cliente2);
-
+    public List<Cliente> listar() {
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 
 }
