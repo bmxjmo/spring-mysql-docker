eb9174e011e916af853d677fc9525b1600e9f238
user <uabj8000@gmail.com>
2020-11-09 20:46:03 -0300

    feat ordens-servico, new controller

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
@@ -1,12 +1,16 @@
 package com.algaworks.osworks.api.controller;
 
 import com.algaworks.osworks.domain.model.OrdemServico;
+import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.GestaoOrdemServicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ordens-servico")
@@ -15,10 +19,29 @@ public class OrdemServicoController {
     @Autowired
     private GestaoOrdemServicoService gestaoOrdemServicoService;
 
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico){
         return gestaoOrdemServicoService.criar(ordemServico);
+    }
+
+    @GetMapping
+    public List<OrdemServico> listar() {
+        return ordemServicoRepository.findAll();
+    }
+
+    @GetMapping("/ordemServicoId")
+    public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemSevicoId) {
+        Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemSevicoId);
+
+        if (ordemServico.isPresent()) {
+            return ResponseEntity.ok(ordemServico.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
